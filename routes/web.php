5211c6f9699c573b49e91cc9f116346f94a85918
Bhatti Faez <faezbhatti@gmail.com>
2021-10-19 09:15:42 +0200

    Test route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test/{name}', function(\Illuminate\Http\Request $request, $name){
+    dd($request);
+});
